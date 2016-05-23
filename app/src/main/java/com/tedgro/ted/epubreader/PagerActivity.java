7c20ad50211426ddeafa6d2dd235203ec0b1d1b0
TedStevensHub <tedstevens@comcast.net>
2016-05-22 21:24:28 -0700

    hi

--- app/src/main/java/com/tedgro/ted/epubreader/PagerActivity.java
+++ app/src/main/java/com/tedgro/ted/epubreader/PagerActivity.java
@@ -142,53 +142,58 @@ public class PagerActivity extends FragmentActivity {
         public ArrayList<Spannable> initiateBook(ArrayList<String> spineList, ArrayList<String> idList, ArrayList<String> pathList, final String resources_path, ViewPager myPager) {
             try {
                 Log.d("pagerview", "#6");
+                ArrayList<Spanned> htmlSpannedArray;
                 strToSpanned str = new strToSpanned();
-                Spanned myspan = str.strToSpanned(spineList, idList, pathList, resources_path, myPager);
+                htmlSpannedArray = str.strToSpanned(spineList, idList, pathList, resources_path, myPager);
                 Log.d("pagerview", "#13");
                 int boundsHeight = myPager.getHeight() - padding;
                 Log.d("pagerview", "#13.1");
-                int newPageLineBottom = 0;
-                int lastPageLineBottom = 0;
-                //myPager is wrong, that refrences a layout without a textview
-                /*
-                NEED A PREPPING FRAGMENT TO DUMP SPANNED INTO A TEXTVIEW
-                */
+
                 TextView prepTextView = (TextView) myPager.findViewById(R.id.prepTextView);
                 Log.d("pagerview", "#13.2");
 //                prepTextView.setVisibility(View.GONE);
-                Log.d("pagerview", "#13.3");
-                prepTextView.setText(myspan);
-                Log.d("pagerview", "#13.4");
-                int totalNumLines = prepTextView.getMaxLines();
-                Log.d("pagerview", "#13.5");
-                int startLine = 1;
-                Spannable addpage = null;
-                Log.d("pagerview", "#14");
 
-                //does getlinebottom return heigh or total distance from top of textview
-                for (int i = 1; i <= totalNumLines; i++) {
-                    newPageLineBottom = prepTextView.getLayout().getLineBottom(i);
+                for (int r=0;r<htmlSpannedArray.size();r++) {
+                    Log.d("pagerview", "#13.3");
+                    //one htmls span
+                    Spanned myspan = htmlSpannedArray.get(r);
+                    Log.d("pagerview", "#13.4");
+                    int newPageLineBottom = 0;
+                    int lastPageLineBottom = 0;
+                    prepTextView.setText(myspan);
+                    Log.d("pagerview", "#13.5");
+                    Log.d("pagerview", "#13.6");
+                    int totalNumLines = prepTextView.getMaxLines();
+                    Log.d("pagerview", "#13.7");
+                    int startLine = 1;
+                    Spannable addpage = null;
+                    Log.d("pagerview", "#14");
 
-                    if (i == totalNumLines && newPageLineBottom - lastPageLineBottom <= boundsHeight) {
-                        int start = prepTextView.getLayout().getLineStart(startLine);
-                        int end = prepTextView.getLayout().getLineEnd(i);
-                        TextUtils.copySpansFrom(myspan, start, end, null, addpage, 0);
-                        pageArray.add(addpage);
-                        addpage = null;
+                    //does getlinebottom return heigh or total distance from top of textview
+                    for (int i = 1; i <= totalNumLines; i++) {
+                        newPageLineBottom = prepTextView.getLayout().getLineBottom(i);
+
+                        if (i == totalNumLines && newPageLineBottom - lastPageLineBottom <= boundsHeight) {
+                            int start = prepTextView.getLayout().getLineStart(startLine);
+                            int end = prepTextView.getLayout().getLineEnd(i);
+                            TextUtils.copySpansFrom(myspan, start, end, null, addpage, 0);
+                            pageArray.add(addpage);
+                            addpage = null;
+                        }
+
+                        if (newPageLineBottom - lastPageLineBottom > boundsHeight) {
+                            i = i - 1;
+                            lastPageLineBottom = prepTextView.getLayout().getLineBottom(i);
+                            int start = prepTextView.getLayout().getLineStart(startLine);
+                            int end = prepTextView.getLayout().getLineEnd(i);
+                            startLine = i + 1;
+                            TextUtils.copySpansFrom(myspan, start, end, null, addpage, 0);
+                            pageArray.add(addpage);
+                            addpage = null;
+                            i += 1;
+                        }
+
                     }
-
-                    if (newPageLineBottom - lastPageLineBottom > boundsHeight) {
-                        i = i - 1;
-                        lastPageLineBottom = prepTextView.getLayout().getLineBottom(i);
-                        int start = prepTextView.getLayout().getLineStart(startLine);
-                        int end = prepTextView.getLayout().getLineEnd(i);
-                        startLine = i + 1;
-                        TextUtils.copySpansFrom(myspan, start, end, null, addpage, 0);
-                        pageArray.add(addpage);
-                        addpage = null;
-                        i += 1;
-                    }
-
                 }
                 prepTextView.setText("");
                 Log.d("pagerview", "#15");
@@ -204,90 +209,96 @@ public class PagerActivity extends FragmentActivity {
 
 
     //return spannable of images and styled text
-    public static class strToSpanned
-        {
+    public static class strToSpanned {
 
-            public Spanned strToSpanned(ArrayList<String> spineList, ArrayList<String> idList, ArrayList<String> pathList, final String resources_path, final ViewPager myPager)throws Exception {
+        public ArrayList<Spanned> strToSpanned(ArrayList<String> spineList, ArrayList<String> idList, ArrayList<String> pathList, final String resources_path, final ViewPager myPager) throws Exception {
 
-                Log.d("pagerview", "#7");
-                concatHtmlString concat = new concatHtmlString();
-                String htmlstring = concat.concatHtmlString(spineList, idList, pathList, resources_path);
-                Log.d("pagerview", "#11");
-                Spanned fullspan = Html.fromHtml(htmlstring, new Html.ImageGetter() {
-                @Override
-                public Drawable getDrawable(String source) {
-                    String path = resources_path + "/" + source;
-                    Drawable d = Drawable.createFromPath(path);
+            Log.d("pagerview", "#7");
+            ArrayList<Spanned> htmlSpannedArray = new ArrayList<>();
+            concatHtmlString concat = new concatHtmlString();
+            ArrayList<String> htmlStringArray = concat.concatHtmlString(spineList, idList, pathList, resources_path);
+            Log.d("pagerview", "#11");
 
 
-                    int finalw = 0;
-                    int finalh = 0;
-                    float widthpercentage = 0;
-                    float heightpercentage = 0;
-                    int widthbounds = myPager.getMeasuredWidth() - padding;
-                    int heightbounds = myPager.getMeasuredHeight() - padding;
-                    int truewidth = d.getIntrinsicWidth();
-                    int trueheight = d.getIntrinsicHeight();
+            for (int i = 0; i < htmlStringArray.size(); i++) {
+                htmlSpannedArray.add(Html.fromHtml(htmlStringArray.get(i), new Html.ImageGetter() {
+                    @Override
+                    public Drawable getDrawable(String source) {
+                        String path = resources_path + "/" + source;
+                        Drawable d = Drawable.createFromPath(path);
 
-                    //scaling
 
-                    widthpercentage = (truewidth - widthbounds) / truewidth;
-                    heightpercentage = (trueheight - heightbounds) / trueheight;
+                        int finalw = 0;
+                        int finalh = 0;
+                        float widthpercentage = 0;
+                        float heightpercentage = 0;
+                        int widthbounds = myPager.getMeasuredWidth() - padding;
+                        int heightbounds = myPager.getMeasuredHeight() - padding;
+                        int truewidth = d.getIntrinsicWidth();
+                        int trueheight = d.getIntrinsicHeight();
 
-                    if (widthpercentage == heightpercentage && widthpercentage >= 0) {
-                        finalw = widthbounds;
-                        finalh = heightbounds;
-                    } else if (widthpercentage == heightpercentage && widthpercentage < 0) {
-                        finalw = truewidth;
-                        finalh = trueheight;
-                    } else if (widthpercentage > heightpercentage) {
-                        if (widthpercentage <= 0) {
-                            //no scaling
+                        //scaling
+
+                        widthpercentage = (truewidth - widthbounds) / truewidth;
+                        heightpercentage = (trueheight - heightbounds) / trueheight;
+
+                        if (widthpercentage == heightpercentage && widthpercentage >= 0) {
+                            finalw = widthbounds;
+                            finalh = heightbounds;
+                        } else if (widthpercentage == heightpercentage && widthpercentage < 0) {
                             finalw = truewidth;
                             finalh = trueheight;
-                        } else {
-                            //do scalling
-                            finalw = (int) (truewidth - (truewidth * widthpercentage));
-                            //use the dominant percentage that is width
-                            finalh = (int) (trueheight - (trueheight * widthpercentage));
+                        } else if (widthpercentage > heightpercentage) {
+                            if (widthpercentage <= 0) {
+                                //no scaling
+                                finalw = truewidth;
+                                finalh = trueheight;
+                            } else {
+                                //do scaling
+                                finalw = (int) (truewidth - (truewidth * widthpercentage));
+                                //use the dominant percentage that is width
+                                finalh = (int) (trueheight - (trueheight * widthpercentage));
+                            }
+                        } else if (widthpercentage < heightpercentage) {
+                            if (heightpercentage <= 0) {
+                                finalw = truewidth;
+                                finalh = trueheight;
+                            } else {
+                                finalw = (int) (truewidth - (truewidth * heightpercentage));
+                                //use the dominant percentage that is height
+                                finalh = (int) (trueheight - (trueheight * heightpercentage));
+                            }
                         }
-                    } else if (widthpercentage < heightpercentage) {
-                        if (heightpercentage <= 0) {
-                            finalw = truewidth;
-                            finalh = trueheight;
-                        } else {
-                            finalw = (int) (truewidth - (truewidth * heightpercentage));
-                            //use the dominant percentage that is height
-                            finalh = (int) (trueheight - (trueheight * heightpercentage));
-                        }
+
+                        d.setBounds(0, 0, finalw, finalh);
+                        return d;
                     }
-
-                    d.setBounds(0, 0, finalw, finalh);
-                    return d;
-                }
-            }, null);
+                }, null));
                 Log.d("pagerview", "#12");
 
-            return fullspan;
 
-
+            }
+            return htmlSpannedArray;
         }
     }
 
     public static class concatHtmlString {
 
-        public String concatHtmlString(ArrayList<String> spineList, ArrayList<String> idList, ArrayList<String> pathList, final String resources_path) throws Exception {
+        public ArrayList<String> concatHtmlString(ArrayList<String> spineList, ArrayList<String> idList, ArrayList<String> pathList, final String resources_path) throws Exception {
             Log.d("pagerview", "#8");
             ArrayList<String> spinePathOrderedList;
             spinePathOrdered spo = new spinePathOrdered();
             spinePathOrderedList = spo.spinePathOrdered(spineList, idList, pathList, resources_path);
             Log.d("pagerview", "#9");
-            String htmlstring = "";
+            ArrayList<String> htmlStringArray = new ArrayList<>();
             for (int i = 0; i < spinePathOrderedList.size(); i++) {
                 Log.d("pagerview", spinePathOrderedList.get(i));
-                htmlstring += getStringFromFile(spinePathOrderedList.get(i));
+
+                //breaks here
+                htmlStringArray.add(getStringFromFile(spinePathOrderedList.get(i)));
+                Log.d("html string debug", "#9.1 Do I have string: "+htmlStringArray.get(i).toString());
             }
-            return htmlstring;
+            return htmlStringArray;
         }
     }
 
@@ -315,7 +326,7 @@ public class PagerActivity extends FragmentActivity {
 
 
 
-
+//error in the two classes below
 
     public static String convertStreamToString(InputStream is) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
